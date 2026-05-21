81d2cdb5da41f35eb2f6491ecc374a297e41ffd1
Heush256 <ktoromani10@gmail.com>
2026-05-21 18:07:51 -0400

    Update Action.js

--- Action.js
+++ Action.js
@@ -1,17 +1,14 @@
-// document.write(`
-// <ul>
-//   <li><a href="/" id="home">Home</a></li>
-//   <li><a href="/Resume/" id="res">Resume</a></li>
-//   <li><a href="/Education/" id="edu">Education</a></li>
-//   <li><a href="/Image/" id="image">Picture</a></li>
-// </ul>
-// `);
+const isSubfolder = window.location.pathname.includes('/Image/') ||
+    window.location.pathname.includes('/Resume/') ||
+    window.location.pathname.includes('/Education/');
+
+const prefix = isSubfolder ? "../" : "";
 
 document.write(`
 <ul>
-  <li><a href="/" id="home">Home</a></li>
-  <li><a href="/Resume/" id="res">Resume</a></li>
-  <li><a href="/Education/" id="edu">Education</a></li>
-  <li><a href="/Image/" id="image">Picture</a></li>
+  <li><a href="${prefix}index.html" id="home">Home</a></li>
+  <li><a href="${prefix}Resume/index.html" id="res">Resume</a></li>
+  <li><a href="${prefix}Education/index.html" id="edu">Education</a></li>
+  <li><a href="${prefix}Image/index.html" id="image">Picture</a></li>
 </ul>
 `);
