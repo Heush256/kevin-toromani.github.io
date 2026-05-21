47a06532f68703c4e0223ee0623b0769a8c12d38
Heush256 <ktoromani10@gmail.com>
2026-05-21 17:28:16 -0400

    Add files via upload

---
+++ Action.js
@@ -0,0 +1,17 @@
+// document.write(`
+// <ul>
+//   <li><a href="/" id="home">Home</a></li>
+//   <li><a href="/Resume/" id="res">Resume</a></li>
+//   <li><a href="/Education/" id="edu">Education</a></li>
+//   <li><a href="/Image/" id="image">Picture</a></li>
+// </ul>
+// `);
+
+document.write(`
+<ul>
+  <li><a href="/Git/" id="home">Home</a></li>
+  <li><a href="/Git/Resume/" id="res">Resume</a></li>
+  <li><a href="/Git/Education/" id="edu">Education</a></li>
+  <li><a href="/Git/Image/" id="image">Picture</a></li>
+</ul>
+`);
