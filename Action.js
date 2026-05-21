7076eedc8932c526747206fada0ca25da2eaef7e
Heush256 <ktoromani10@gmail.com>
2026-05-21 18:58:41 -0400

    Add files via upload

--- Action.js
+++ Action.js
@@ -5,10 +5,12 @@ const isSubfolder = window.location.pathname.includes('/Image/') ||
 const prefix = isSubfolder ? "../" : "";
 
 document.write(`
-<ul>
-  <li><a href="${prefix}index.html" id="home">Home</a></li>
-  <li><a href="${prefix}Resume/index.html" id="res">Resume</a></li>
-  <li><a href="${prefix}Education/index.html" id="edu">Education</a></li>
-  <li><a href="${prefix}Image/index.html" id="image">Picture</a></li>
-</ul>
+<div id="Container" style="height: auto">
+    <ul>
+      <li><a href="${prefix}index.html" id="home">Home</a></li>
+      <li><a href="${prefix}Resume/index.html" id="res">Resume</a></li>
+      <li><a href="${prefix}Education/index.html" id="edu">Education</a></li>
+      <li><a href="${prefix}Image/index.html" id="image">Picture</a></li>
+    </ul>
+</div>
 `);
